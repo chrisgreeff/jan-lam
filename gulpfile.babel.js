3d5e997e8c2ddf4d8e07273bcffe94d6efbc6ed0
Chris Greeff <chris.r.greeff@gmail.com>
2015-07-18 18:02:09 +1200

    Touching up header layout

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,11 @@
 import gulp from 'gulp'
 import sass from 'gulp-sass'
+import autoprefixer from 'gulp-autoprefixer'
 
 gulp.task('sass', () => {
   gulp.src('assets/scss/**/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer())
     .pipe(gulp.dest('web/css'))
 })
 
